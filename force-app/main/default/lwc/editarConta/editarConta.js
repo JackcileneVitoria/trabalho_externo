aac5801f11bc4703da8c9d4af9e4f34a5864a111
Jackcilene Vitória <jackcilenevtoria@gmail.com>
2020-11-03 02:31:19 -0300

    inclusão de alertas no lwc

--- force-app/main/default/lwc/editarConta/editarConta.js
+++ force-app/main/default/lwc/editarConta/editarConta.js
@@ -1,11 +1,24 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+const campos = ['Account.invalidField' ];
 export default class EditarConta extends LightningElement {
     @api recordId;
-    handleSubmit(event) {
-        console.log('onsubmit event recordEditForm'+ event.detail.fields);
+    handleError(event) {
+        this.exibirAlerta('error', 'Erro', event.detail.detail);
     }
+
     handleSuccess(event) {
-        console.log('onsuccess event recordEditForm', event.detail.id);
+        this.exibirAlerta('success', 'Sucesso', 'Informações atualizadas com sucesso!');
     }
+
+    // Alertas
+    exibirAlerta(tipo, titulo, mensagem) {
+        this.dispatchEvent(new ShowToastEvent({ 
+            title: titulo, 
+            message: mensagem, 
+            variant: tipo }));
+    }
+
 }
